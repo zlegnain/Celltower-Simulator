b7d31fd058834c0e2aeb4047e65280493737c841
zlegnain <90531170+zlegnain@users.noreply.github.com>
2021-10-10 17:35:56 -0700

    Update vehicle.c

--- vehicle.c
+++ vehicle.c
@@ -35,14 +35,14 @@ int main(int argc, char * argv[]) {
 
   while((x<CITY_WIDTH && y<CITY_HEIGHT) || connectedTowerID) {
 
-    /*generate a random number*/
+    //generate a random number
     int rannum = rand()%3;
 
-    /*vehicle turn less*/
+    //vehicle turn less
     if(rannum==0){
       direction = direction - VEHICLE_TURN_ANGLE;
     }
-    /*vehicle turn more*/
+    //vehicle turn more
     else if(rannum==1){
       direction = direction + VEHICLE_TURN_ANGLE;
     }
@@ -59,7 +59,7 @@ int main(int argc, char * argv[]) {
     usleep(50000);
 
 
-    /*create client server*/
+    //create client server
     int serverSocket, clientSocket;
     struct sockaddr_in serverAddress, clientAddr;
     int status, addrSize, bytesRcv;
@@ -67,11 +67,11 @@ int main(int argc, char * argv[]) {
 
 
 
-    /*check if connected to tower*/
+    //check if connected to tower
     if(connectedTowerID<0){
       for(int i=0;i<NUM_TOWERS;i++){
 
-        /*convert short to byte array*/
+        //convert short to byte array
         buffer[0] = CONNECT;
         buffer[1] = (unsigned char)(x/256);
         buffer[2] = (unsigned char)x%256;
@@ -79,14 +79,14 @@ int main(int argc, char * argv[]) {
         buffer[4] = (unsigned char)y%256;
 
 
-        /*set up server address*/
+        //set up server address
         clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
         memset(&serverAddress, 0, sizeof(serverAddress));
         serverAddress.sin_family = AF_INET;
         serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
         serverAddress.sin_port = htons((unsigned short) SERVER_PORT + i);
 
-        /*connect to the server*/
+        //connect to the server
         status = connect(clientSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
 
         if(status<0){
@@ -95,10 +95,10 @@ int main(int argc, char * argv[]) {
         }
 
 
-        /*send array of data to server*/
+        //send array of data to server
         send(clientSocket, buffer, 5,0);
 
-        /*get response from server*/
+        //get response from server
         bytesRcv = recv(clientSocket, buffer, sizeof(buffer),0);
         buffer[bytesRcv] = 0;
         if(buffer[0]==YES){
@@ -112,7 +112,7 @@ int main(int argc, char * argv[]) {
       }
     }else{
 
-      /*convert short to byte array*/
+      //convert short to byte array
       buffer[0] = UPDATE;
       buffer[1] = (unsigned char)(x/256);
       buffer[2] = (unsigned char)x%256;
@@ -120,23 +120,23 @@ int main(int argc, char * argv[]) {
       buffer[4] = (unsigned char)y%256;
       buffer[5] = connectionID;
 
-      /*set up server address*/
+      //set up server address
       clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
       memset(&serverAddress, 0, sizeof(serverAddress));
       serverAddress.sin_family = AF_INET;
       serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
       serverAddress.sin_port = htons((unsigned short) SERVER_PORT + connectedTowerID);
 
-      /*connect to the server*/
+      //connect to the server
       status = connect(clientSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
-      /*check if status is error*/
+      //check if status is error
       if(status < 0){
         close(clientSocket);
         exit(0);
       }
-      /*send array of data to server*/
+      //send array of data to server
       send(clientSocket, buffer, 6,0);
-      /*get response from server*/
+      //get response from server
       bytesRcv = recv(clientSocket, buffer, sizeof(buffer),0);
       buffer[bytesRcv] = 0;
       if(buffer[0]==YES){
